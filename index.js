3b40f1150706c966821c25a42c3af93b50457f51
Rojin <rojin007.rr@gmail.com>
2022-09-30 18:04:46 +0530

    database started

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const { generateAccessToken } = require("./jwt");
+
+const express = require("express");
 const http = require("http");
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end("Hello, World!");
 };
 const healer = "debugger";
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/authentication");
+var app = express();
+//app.post('/addUser',async(req,res)=>{const (user.name)req.body.name});
+
 const heal = generateAccessToken(healer);
 console.log(heal);
 
---
+++ models/index.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const User = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model(User);
