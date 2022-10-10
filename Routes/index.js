0de46a392b1eb152cd04ac7afe8be4a09d590f3e
Rojin <rojin007.rr@gmail.com>
2022-10-10 18:07:46 +0530

    test

--- Routes/index.js
+++ Routes/index.js
@@ -3,4 +3,4 @@ const { CreateUser } = require("./controllers/signup");
 const express = require("express");
 const router = express.Router();
 
-router.post("/signup", CreateUser);
+router.route("/signup").post(CreateUser);
